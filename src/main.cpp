45bfde2f78b23053145e3029b292a8198d07a6ea
chakku <chakkurai0724@outlook.jp>
2018-11-22 15:36:49 +0900

    Wifi接続をWiFi.hで定義されているWiFiを使用するように変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //#include <Arduino.h>
 #include <WiFi.h>
-#include <WiFiMulti.h>
+#include <WiFiUdp.h>
 #include <HTTPClient.h>
 #include <M5Stack.h>
 
@@ -8,21 +8,31 @@
 
 const char* ssid = SSID;
 const char* password = PASSWORD;
+const char* udp_address = "192.168.0.255";
+const int udp_port = 3333;
+bool wifi_connected = false;
 
 HTTPClient http;
-WiFiMulti wifi;
+WiFiUDP udp;
+
+void connect_WiFi(){
+    WiFi.disconnect(true);
+    WiFi.begin(ssid,password);
+
+    while(WiFi.status() != WL_CONNECTED){
+        delay(1000);
+        M5.Lcd.println("WiFi connecting...");
+    }
+    M5.Lcd.println("WiFi Connected!");
+}
+
 
 void setup() {
-    wifi.addAP(ssid,password);
     Serial.begin(115200);
     M5.begin();
 
     // WIFI接続完了を待つ
-    while(wifi.run() != WL_CONNECTED){
-        delay(500);
-        M5.Lcd.printf("WiFi Connecting...");
-    }
-    M5.Lcd.println("WiFi Connected!");
+    connect_WiFi();
 }
 
 void loop() {
