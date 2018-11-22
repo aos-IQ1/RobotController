c5776881c8a6370f6c3125be556d8d338f110409
chakku <chakkurai0724@outlook.jp>
2018-11-22 14:41:25 +0900

    となりでAshur_titechがなんか言ってるので(笑)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+
+#include "password.h"
+
+const char* ssid = SSID;
+const char* password = PASSWORD;
+
+HTTPClient http;
 
 void setup() {
     // put your setup code here, to run once:
