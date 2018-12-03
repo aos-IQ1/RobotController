2a4631cfe7d4a4349b08aec4c94904f92ae9a130
chakku <chakkurai0724@outlook.jp>
2018-12-03 15:50:37 +0900

    ジャイロと加速度の値を取得

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ bool wifi_connected = false;
 
 WiFiUDP udp;
 
-//MPU9250 IMU;
+MPU9250 IMU;
 
 void connect_WiFi(){
     WiFi.disconnect(true);
@@ -46,6 +46,29 @@ void button_controller(){
 }
 
 void acc_sensor_controller(){
+    if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+    {
+        IMU.readAccelData(IMU.accelCount);
+        IMU.getAres();                                // IMU.aresが更新される ares : Scale resolutions per LSB for the sensors
+        IMU.ax = (float)IMU.accelCount[0] * IMU.aRes; // - accelBias[0];
+        IMU.ay = (float)IMU.accelCount[1] * IMU.aRes; // - accelBias[1];
+        IMU.az = (float)IMU.accelCount[2] * IMU.aRes; // - accelBias[2];
+        IMU.readGyroData(IMU.gyroCount);              // Read the x/y/z adc values
+        IMU.getGres();
+
+        // Calculate the gyro value into actual degrees per second
+        // This depends on scale being set
+        IMU.gx = (float)IMU.gyroCount[0] * IMU.gRes;
+        IMU.gy = (float)IMU.gyroCount[1] * IMU.gRes;
+        IMU.gz = (float)IMU.gyroCount[2] * IMU.gRes;
+    }
+    IMU.updateTime();
+    M5.Lcd.print("ax = ");
+    M5.Lcd.println((int)IMU.gx);
+    M5.Lcd.print("ay = ");
+    M5.Lcd.println((int)IMU.gy);
+    M5.Lcd.print("az = ");
+    M5.Lcd.println((int)IMU.gz);
     /*
     if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01){
         IMU.readAccelData(IMU.accelCount);
@@ -71,9 +94,13 @@ void acc_sensor_controller(){
 }
 
 void setup() {
+    delay(200);
     Serial.begin(115200);
     M5.begin();
+    Wire.begin();
     Serial.println("Start");
+
+    M5.Lcd.println("This is Robot Controller");
 
     // WIFI接続完了を待つ
     //connect_WiFi();
@@ -82,19 +109,21 @@ void setup() {
     //udp.begin(udp_port);
 
     // センサーの初期化
-    //IMU.calibrateMPU9250(IMU.gyroBias,IMU.accelBias);
+    IMU.calibrateMPU9250(IMU.gyroBias,IMU.accelBias);
+    IMU.initMPU9250();
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0,0);
     //M5.update();
-    //M5.Lcd.println("Looping");
+    M5.Lcd.println("Looping");
 
     /* ボタンを使ったコントローラ */
     //button_controller();
 
     /* 加速度センサを使ったコントローラ */
-    //acc_sensor_controller();
-
-    delay(300);
+    acc_sensor_controller();
+    delay(1000);
 }
